58c94f5cf08f155957b0a60157c4736c5e32fcf2
Andrew McDonald <e_dorion@hotmail.com>
2013-05-07 14:55:22 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WattleParkGolfClub::Application.config.secret_token = 'edfc50874908a73d5bd4051dee4c4dce1e6cc6f661a1a48d076178efefece187696c951bb8b55daaa0d0dc4301da98df7c8ac2092e579eded0bccff855616383'

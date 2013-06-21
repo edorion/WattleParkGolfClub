ba5adb182cd729acd187293e4e4854ae7c32af0c
Andrew McDonald <e_dorion@hotmail.com>
2013-06-22 05:51:35 +1000

    changed logo

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -8,7 +8,8 @@
 Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
-  # config.site_name = "Spree Demo Site"
+ config.site_name = "Wattle Park Golf Club"
+ config.set(logo: "store/golf logo.jpg")
 end
 
 Spree.user_class = "Spree::User"
